dda95dd5697bcfc360210deffb480783590e3537
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-01 19:00:55 +0530

    Okay Guys

--- aMusic/ViewController.swift
+++ aMusic/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var table:UITableView!
+    
+    var songs = [Song]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let position = indexPath.row
+        
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player")
+            else {
+                return
+            }
+        present(vc, animated: true)
+    }
+}
 
-
+struct Song {
+    
 }
 
