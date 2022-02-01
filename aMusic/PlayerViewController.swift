f2253f55afe83e7bdbb3ef963d4ce88a3774eddc
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-01 20:47:08 +0530

    Added Slider and buttons

--- aMusic/PlayerViewController.swift
+++ aMusic/PlayerViewController.swift
@@ -16,6 +16,32 @@ class PlayerViewController: UIViewController {
     
     var player: AVAudioPlayer?
     
+    
+    private let albumImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let songNameLabel: UILabel = {
+       let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    private let artistNameLabel: UILabel = {
+       let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    private let albumNameLabel: UILabel = {
+       let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -47,13 +73,79 @@ class PlayerViewController: UIViewController {
                 return
             }
             
+            player.volume = 0.5
+            
             player.play()
 
         } catch {
             print("error occurred")
         }
         
-        // setup user player
+        // setup user interface elements
+        
+        
+        // album Cover
+        albumImageView.frame = CGRect(x: 10,
+                                     y: 10,
+                                     width: holder.frame.size.width - 20,
+                                     height: holder.frame.size.width - 20
+        )
+        albumImageView.image = UIImage(named: song.imageName)
+        holder.addSubview(albumImageView)
+        
+        // labels
+        songNameLabel.frame = CGRect(x: 10,
+                                     y: albumImageView.frame.size.height + 10,
+                                     width: holder.frame.size.width - 20,
+                                     height: 70
+        )
+        
+        albumNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height + 10 + 70,
+                                      width: holder.frame.size.width - 20,
+                                      height: 70
+        )
+        
+        artistNameLabel.frame = CGRect(x: 10,
+                                       y: albumImageView.frame.size.height + 10 + 140,
+                                       width: holder.frame.size.width - 20,
+                                       height: 70
+        )
+        
+        songNameLabel.text = song.name
+        albumNameLabel.text = song.albumName
+        artistNameLabel.text = song.artistName
+        
+        holder.addSubview(songNameLabel)
+        holder.addSubview(albumNameLabel)
+        holder.addSubview(artistNameLabel)
+        
+        
+        // Player Controls
+        let playPauseButton = UIButton()
+        let nextButton = UIButton()
+        let prevButton = UIButton()
+
+        
+        
+        //slider
+        
+        let slider = UISlider(frame: CGRect(x: 20,
+                                            y: holder.frame.size.height-60,
+                                            width: holder.frame.size.width-40,
+                                            height: 50))
+        slider.value = 0.5
+        slider.addTarget(self, action: #selector(didSlideSlider(_:)), for: .valueChanged)
+        holder.addSubview(slider)
+        
+        
+    }
+    
+    @objc func didSlideSlider(_ slider: UISlider){
+        let value = slider.value
+        player?.volume = value
+        // adjust player volume
+        
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
